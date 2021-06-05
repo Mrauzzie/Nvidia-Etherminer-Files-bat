f70312e5c80914554305fea225e3b45bc8b82fe2
Mrauzzie <78454466+Mrauzzie@users.noreply.github.com>
2021-06-05 12:26:04 -0400

    Add files via upload

---
+++ ethminer-0.18.0-cuda10.0-windows-amd64/bin/loop.bat
@@ -0,0 +1,5 @@
+cd C:\Users\auzzi\Desktop\Mining\ethminer-0.18.0-cuda10.0-windows-amd64\bin
+
+:loop
+Start start.bat | set /P "="
+goto loop
---
+++ ethminer-0.18.0-cuda10.0-windows-amd64/bin/start.bat
@@ -0,0 +1,4 @@
+
+ethminer.exe -P stratum1+tcp://0xE04B274800f046B79f205a3395B2E67510F0f721.miner:auzziewildcat10@gmail.com@us1.ethermine.org:4444 -U -R
+
+exit
