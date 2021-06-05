105e29ff2c5fe73c27fd81549d8a8395985ca8cd
Mrauzzie <78454466+Mrauzzie@users.noreply.github.com>
2021-06-05 12:27:30 -0400

    Update start.bat

--- ethminer-0.18.0-cuda10.0-windows-amd64/bin/start.bat
+++ ethminer-0.18.0-cuda10.0-windows-amd64/bin/start.bat
@@ -1,4 +1,4 @@
 
-ethminer.exe -P stratum1+tcp://0xE04B274800f046B79f205a3395B2E67510F0f721.miner:auzziewildcat10@gmail.com@us1.ethermine.org:4444 -U -R
+ethminer.exe -P stratum1+tcp://YOURWALLETRECIEVEADDRESSHERE.WORKERNAMEHERE:EMAILHERE/PASSWORD@us1.ethermine.org:4444 -U -R
 
 exit
